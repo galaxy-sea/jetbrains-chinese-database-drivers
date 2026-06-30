e312a262af95b7c165f8f0e8e4dffb98c3f4e70a
ChangJin Wei (魏昌进) <WeiChangJin1996@gmail.com>
2026-07-01 01:15:55 +0800

    openGauss 包定义问题，----牛逼的语义化版本号！！！

--- opengauss-driver-integration/build.gradle.kts
+++ opengauss-driver-integration/build.gradle.kts
@@ -3,7 +3,7 @@ import plus.wcj.gradle.DatabaseArtifactConfigExtension
 extensions.configure<DatabaseArtifactConfigExtension>("databaseArtifactConfig") {
     mavenArtifacts.set(
         listOf(
-            mavenArtifact("openGauss Driver", "org.opengauss:opengauss-jdbc", 2, ".*-compatibility$"),
+            mavenArtifact("openGauss Driver", "org.opengauss:opengauss-jdbc", 2, "^(?!.*-og).*$"),
             mavenArtifact("GaussDB Driver", "com.huaweicloud:gaussdbjdbc", 2, "^505.*", "^V2.*")
         )
     )
