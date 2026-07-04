0fc03c0c8b8bbd5f50b2d8aa906fb66767b6f804
ChangJin Wei (魏昌进) <WeiChangJin1996@gmail.com>
2026-07-04 16:08:06 +0800

    更新一些元数据

--- mudb-driver-integration/build.gradle.kts
+++ mudb-driver-integration/build.gradle.kts
@@ -4,5 +4,5 @@ import plus.wcj.gradle.DatabaseArtifactConfigExtension
 // https://repo.maven.apache.org/maven2/org/opengauss/opengauss-jdbc/maven-metadata.xml
 
 extensions.configure<DatabaseArtifactConfigExtension>("databaseArtifactConfig") {
-    mavenArtifacts.set(listOf(mavenArtifact("MuDB Driver", "org.opengauss:opengauss-jdbc")))
+    mavenArtifacts.set(listOf(mavenArtifact("MuDB Driver", "org.opengauss:opengauss-jdbc", "org.opengauss:opengauss-jdbc", 2, "^(?!.*-og).*$", ".*RC.*")))
 }
