a13c407863a72eb932eac812d5f2c7b7ddfb8232
ChangJin Wei (魏昌进) <WeiChangJin1996@gmail.com>
2026-07-05 00:17:37 +0800

    优化 jdbc 版本 ，icon同步

--- apachekylin-driver-integration/build.gradle.kts
+++ apachekylin-driver-integration/build.gradle.kts
@@ -4,5 +4,5 @@ import plus.wcj.gradle.DatabaseArtifactConfigExtension
 // https://repo.maven.apache.org/maven2/org/apache/kylin/kylin-jdbc/maven-metadata.xml
 
 extensions.configure<DatabaseArtifactConfigExtension>("databaseArtifactConfig") {
-    mavenArtifacts.set(listOf(mavenArtifact("Apache Kylin Driver", "org.apache.kylin:kylin-jdbc")))
+    mavenArtifacts.set(listOf(mavenArtifact("Apache Kylin Driver", "org.apache.kylin:kylin-jdbc", 2, ".*[A-Za-z].*")))
 }
--- databend-driver-integration/build.gradle.kts
+++ databend-driver-integration/build.gradle.kts
@@ -4,5 +4,6 @@ import plus.wcj.gradle.DatabaseArtifactConfigExtension
 // https://repo.maven.apache.org/maven2/com/databend/databend-jdbc/maven-metadata.xml
 
 extensions.configure<DatabaseArtifactConfigExtension>("databaseArtifactConfig") {
-    mavenArtifacts.set(listOf(mavenArtifact("Databend Driver", "com.databend:databend-jdbc")))
+
+    mavenArtifacts.set(listOf(mavenArtifact("Databend Driver", "com.databend:databend-jdbc", 2, ".*[A-Za-z].*")))
 }
