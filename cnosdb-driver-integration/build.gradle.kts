f76496ffee6ac4fa733938a2baea73d0d266542b
ChangJin Wei (魏昌进) <WeiChangJin1996@gmail.com>
2026-07-10 11:27:24 +0800

    CnosDB arrow-jdbc

--- cnosdb-driver-integration/build.gradle.kts
+++ cnosdb-driver-integration/build.gradle.kts
@@ -4,5 +4,5 @@ import plus.wcj.gradle.DatabaseArtifactConfigExtension
 // https://repo.maven.apache.org/maven2/org/apache/arrow/flight-sql-jdbc-driver/maven-metadata.xml
 
 extensions.configure<DatabaseArtifactConfigExtension>("databaseArtifactConfig") {
-    mavenArtifacts.set(listOf(mavenArtifact("CnosDB Driver", "org.apache.arrow:flight-sql-jdbc-driver",2)))
+    mavenArtifacts.set(listOf(mavenArtifact("CnosDB Driver", "org.apache.arrow:flight-sql-jdbc-driver", 1, true)))
 }
